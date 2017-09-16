cdcd967a7183206cc22e5c436efe4b87d8a6d0fd
Adil Moujahid <adil.mouja@gmail.com>
2017-09-17 01:58:59 +0900

    Updated script to work with Tensorflow

--- install-gpu.sh
+++ install-gpu.sh
@@ -17,41 +17,27 @@ sudo apt-get -y install cuda
 sudo modprobe nvidia
 nvidia-smi
 
-# install Anaconda for current user
-mkdir downloads
-cd downloads
-wget "https://repo.continuum.io/archive/Anaconda2-4.3.0-Linux-x86_64.sh" -O "Anaconda2-4.3.0-Linux-x86_64.sh"
-bash "Anaconda2-4.3.0-Linux-x86_64.sh" -b
-
-echo "export PATH=\"$HOME/anaconda2/bin:\$PATH\"" >> ~/.bashrc
-export PATH="$HOME/anaconda2/bin:$PATH"
-conda install -y bcolz
-conda upgrade -y --all
-
-# install and configure theano
-pip install theano
-echo "[global]
-device = gpu
-floatX = float32
-[cuda]
-root = /usr/local/cuda" > ~/.theanorc
-
-# install and configure keras
-pip install keras
-mkdir ~/.keras
-echo '{
-    "image_dim_ordering": "th",
-    "epsilon": 1e-07,
-    "floatx": "float32",
-    "backend": "theano"
-}' > ~/.keras/keras.json
-
 # install cudnn libraries
-wget "http://platform.ai/files/cudnn.tgz" -O "cudnn.tgz"
+wget "http://files.fast.ai/files/cudnn-8.0-linux-x64-v6.0.tgz" -O "cudnn.tgz"
 tar -zxf cudnn.tgz
 cd cuda
 sudo cp lib64/* /usr/local/cuda/lib64/
 sudo cp include/* /usr/local/cuda/include/
+
+sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64"
+export CUDA_HOME=/usr/local/cuda
+
+# install Anaconda for current user
+mkdir downloads
+cd downloads
+wget "https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh" -O "Anaconda3-4.4.0-Linux-x86_64.sh"
+bash "Anaconda3-4.4.0-Linux-x86_64.sh" -b
+
+echo "export PATH=\"$HOME/anaconda3/bin:\$PATH\"" >> ~/.bashrc
+export PATH="$HOME/anaconda3/bin:$PATH"
+conda install -y bcolz
+conda upgrade -y --all
 
 # configure jupyter and prompt for password
 jupyter notebook --generate-config
@@ -60,9 +46,17 @@ echo "c.NotebookApp.password = u'"$jupass"'" >> $HOME/.jupyter/jupyter_notebook_
 echo "c.NotebookApp.ip = '*'
 c.NotebookApp.open_browser = False" >> $HOME/.jupyter/jupyter_notebook_config.py
 
-# clone the fast.ai course repo and prompt to start notebook
-cd ~
-git clone https://github.com/fastai/courses.git
-echo "\"jupyter notebook\" will start Jupyter on port 8888"
-echo "If you get an error instead, try restarting your session so your $PATH is updated"
+#Create conda environment called tensorflow
+conda create -n tensorflow
+#Activate tensorflow environment
+source activate tensorflow
+
+#Issue a command of the following format to install TensorFlow inside your conda environment:
+pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.3.0-cp36-cp36m-linux_x86_64.whl
+
+
+#Install keras 
+pip install keras
+
+
 
